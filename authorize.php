128a1da557dc63a5b8cd082c076e485daebecd4d
Pizzaroot <therealpizzaroot@gmail.com>
2016-09-26 16:35:16 +0400

    uploaded files
    uploaded files

---
+++ authorize.php
@@ -0,0 +1,29 @@
+<?php
+$error=''; // Variable To Store Error Message
+if (isset($_POST['submit'])) {
+if (empty($_POST['username']) || empty($_POST['password'])) {
+$error = "Username or Password is invalid";
+}
+else
+{
+// Define $username and $password
+$username=$_POST['username'];
+$password=$_POST['password'];
+// Establishing Connection with Server by passing server_name, user_id and password as a parameter
+$connection = mysql_connect("secret", "secret", "secret");
+// Selecting Database
+$db = mysql_select_db("secret", $connection);
+// SQL query to fetch information of registerd users and finds user match.
+$query = mysql_query("select * from user where password='$password' AND username='$username'", $connection);
+$rows = mysql_num_rows($query);
+if ($rows > 0) {
+	$error = "Congratulations! You have solved the challenge! the secret code is secret";
+} else {
+	$error = "Username or Password is invalid";
+}
+mysql_close($connection); // Closing Connection
+}
+}
+
+echo $error;
+?>
